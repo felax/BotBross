695cbc38847253685f2d1dcbb6b68776e39513b3
von3007 <von3007@live.ca>
2018-01-21 19:41:01 -0500

    Added the play function that calls the bot in the voice channel

--- bot.js
+++ bot.js
@@ -101,6 +101,28 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			case 'leave':
 				bot.leaveVoiceChannel(bot_ID);	
 			break;
+            case 'play':
+                //Let's join the voice channel, the ID is whatever your voice channel's ID is.
+                bot.joinVoiceChannel('403750974205853706', function (error, events) {
+                    //Check to see if any errors happen while joining.
+                    if (error) return console.error(error);
+
+                    //Then get the audio context
+                    bot.getAudioContext('403750974205853706', function (error, stream) {
+                        //Once again, check to see if any errors exist
+                        if (error) return console.error(error);
+
+                        //Create a stream to your file and pipe it to the stream
+                        //Without {end: false}, it would close up the stream, so make sure to include that.
+                        fs.createReadStream('music.mp3').pipe(stream, { end: false });
+
+                        //The stream fires `done` when it's got nothing else to send to Discord.
+                        stream.on('done', function () {
+                            //Handle
+                        });
+                    });
+                });
+                break;
 			case 'day':
 				async.waterfall([
 					function(callback) {
