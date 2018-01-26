fe973305504b2209fbd3edcb79dfc27e66fc7e1e
Charles Piette <charliesis>
2018-01-26 14:11:48 -0500

    Added commands.js

---
+++ commands.js
@@ -0,0 +1,222 @@
+var weather = require('./weather.js');
+var fs = require('fs');
+var league = require('./league.JS');
+var sun = require("./sun.js");
+var rl = require("./rocketleague.js");
+var steam = require("./steam.js");
+const async = require('async');
+
+exports.commands = function(bot, message, user, userID, channelID, evt){
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
+    var parameter = args[1];
+    var parameter2 = args[2];
+
+    // var server_ID = bot.channels[channelID].guild_id;
+    // var user_ID = bot.servers[server_ID].members[userID].voice_channel_id;
+    // var bot_ID = bot.servers[server_ID].members['404808825451970563'].voice_channel_id;
+
+    args = args.splice(1);
+    switch(cmd) {
+        case 'bing':
+            bot.sendMessage({
+                to: channelID,
+                message: '!bong'
+            });
+        break;
+        case 'bong':
+            bot.sendMessage({
+                to: channelID,
+                message: '!bing'
+            });
+        break;
+        case 'reee':
+            bot.sendMessage({
+                to: channelID,
+                tts: true,
+                message: 'REEEEEEEEE'
+            });	
+        break;
+        case 'flip':
+            var rand = Math.random()
+            var answer;
+            if(rand <= 0.5){
+                answer = "Tails";
+            } else {
+                answer = "Heads";
+            }
+            bot.sendMessage({
+                to: channelID,
+                message: answer
+            });	
+        break;
+        case 'roll':
+            if(parameter == null) parameter = 100;
+            var rand = Math.floor(Math.random() * parameter);
+            bot.sendMessage({
+                to: channelID,
+                message: rand
+            });	
+        break;
+        case 'help':
+            bot.sendMessage({
+                to: channelID,
+                message: " !roll {range} \n!flip \n!reee \n!weather {city} \n!ranked {summoner_id} \n!bing \n!day {location} \n!rl {ranked; stats} {steamId} \n!steam {played} {steamId}"
+            });	
+        break;
+        case 'join':
+            bot.joinVoiceChannel(user_ID, function(error, events){
+                if (error) return console.error(error);
+                events.on('speaking', function(userID, SSRC, speakingBool) {
+                    console.log("%s is " + (speakingBool ? "now speaking" : "done speaking"), userID );
+                });
+                bot.getAudioContext(user_ID, function(error, stream){
+                    if (error) return console.error(error);
+                    fs.createReadStream('music.mp3').pipe(stream, { end: false });
+                    stream.on('done', function(){
+                        bot.leaveVoiceChannel(bot_ID);
+                    });
+                });
+            });
+            console.log(user_ID);
+        break;
+        case 'leave':
+            bot.leaveVoiceChannel(bot_ID);	
+        break;
+        case 'play':
+            //Let's join the voice channel, the ID is whatever your voice channel's ID is.
+            bot.joinVoiceChannel('403750974205853706', function (error, events) {
+                //Check to see if any errors happen while joining.
+                if (error) return console.error(error);
+
+                //Then get the audio context
+                bot.getAudioContext('403750974205853706', function (error, stream) {
+                    //Once again, check to see if any errors exist
+                    if (error) return console.error(error);
+
+                    //Create a stream to your file and pipe it to the stream
+                    //Without {end: false}, it would close up the stream, so make sure to include that.
+                    fs.createReadStream('music.mp3').pipe(stream, { end: false });
+
+                    //The stream fires `done` when it's got nothing else to send to Discord.
+                    stream.on('done', function () {
+                        //Handle
+                    });
+                });
+            });
+            break;
+        case 'day':
+            async.waterfall([
+                function(callback) {
+                    sun.getCoords(parameter, (coords) => {
+                        callback(null, coords);
+                    });
+                },
+                function(coords, callback) {
+                    // do some more stuff ..
+                    sun.getDay(coords, (temp) => {
+                        callback(null, temp);
+                    });
+                }
+            ],
+            // optional callback
+            function(err, results) {
+                bot.sendMessage({
+                    to: channelID,
+                    message: results
+                });
+                // results is now equal to ['one', 'two']
+            });
+        break;
+        case 'weather':
+            async.waterfall([
+                function(callback) {
+                    weather.getCoords(parameter, (coords) => {
+                        callback(null, coords);
+                    });
+                },
+                function(coords, callback) {
+                    // do some more stuff ...
+                    weather.getTemp(coords, (temp) => {
+                        callback(null, temp);
+                    });
+                }
+            ],
+            // optional callback
+            function(err, results) {
+                bot.sendMessage({
+                    to: channelID,
+                    message: '**' + results[1] + ', ' + results[2] + '** '+ results[7] +'\n```Temperature: ' + Math.round((results[0]-273.15)).toString() + ' degrees celcius \nSky: ' + results[5] + '\nWind: ' + results[3] + ' Km/h in the ' + results[4] + ' direction\nHumidity: ' + results[6] + '%```'
+                });
+                // results is now equal to ['one', 'two']
+            });
+        break;
+        case 'ranked':
+            async.waterfall([
+                function(callback) {
+                    league.getSummonerId(parameter, (id) => {
+                        console.log("fetching id");
+                        console.log(id);
+                        callback(null, id);
+                    });
+                },
+                function(id, callback) {
+                    league.getRanked(id, (ratio) => {
+                        callback(null, ratio);
+                    });
+                }                    
+            ],
+                // optional callback
+            function(err, results) {
+                if (results[5] ==  true) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: ':fire: :fire: :fire: \n ```Win/Loss: ' + results[0] + '/' + results[1] + " - " + Math.round((results[0]/(results[0] + results[1]))*100, 0)  + "% \n"+
+                                    results[2] + " " + results[3] + " - " + results[4] + " LP```"
+                    });
+                }else{
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '```Win/Loss: ' + results[0] + ' / ' + results[1] + " - " + Math.round((results[0]/(results[0] + results[1]))*100, 0)  + "% \n"+
+                                    results[2] + " " + results[3] + " - " + results[4] + " LP```"
+                    });
+                }
+                if (results[5] ==  true) {
+
+                }
+                // results is now equal to ['one', 'two']
+            });
+        break;
+        case 'rl':
+            async.waterfall([
+                function(callback) {
+                    rl.getPlayer(parameter, parameter2, (stats) => {
+                        callback(null, stats);
+                    });
+                }
+            ],
+            function(err, results) {
+                bot.sendMessage({
+                    to: channelID,
+                    message: results
+                });
+            });
+        break;
+        case 'steam':
+            steam.getPlayer(parameter, parameter2, (stats) => {
+                var msg = "**__" + stats[1] + "__**\n\n";
+                if(stats != "Invalid entry"){
+                    for(var i = 0; i < 3; i++){
+                        msg += "**" + stats[0][i][0] + "**```Last 2 weeks: " + stats[0][i][1] + "Hrs\nAll time: " + stats[0][i][2] + "Hrs```";
+                    }
+                } else {
+                    msg = stats;
+                }
+                bot.sendMessage({
+                    to: channelID,
+                    message: msg
+                });
+            });   
+        break;
+    }
+}
